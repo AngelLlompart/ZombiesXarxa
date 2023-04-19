d089e681f24c5a2fb259116dc13f8215a1b99b47
unknown <angelllompart@paucasesnovescifp.cat>
2023-04-19 11:35:56 +0200

    Added room and lobby (unfinished)

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MainMenuManager : MonoBehaviour
+public class MainMenuManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private TMP_InputField username;
+
+    [SerializeField] private Button multiplayerButton;
+
+    [SerializeField] private TextMeshProUGUI multiplayerButtonText;
     // Start is called before the first frame update
     void Start()
     {
+        if (PhotonNetwork.IsConnected)
+        {
+            StartCoroutine(DisconnectPlayer());
+        }
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
     }
@@ -16,7 +28,14 @@ public class MainMenuManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (username.text.Length >= 1)
+        {
+            multiplayerButton.interactable = true;
+        }
+        else
+        {
+            multiplayerButton.interactable = false;
+        }
     }
 
     public void Play()
@@ -26,6 +45,14 @@ public class MainMenuManager : MonoBehaviour
 
     public void Multiplayer()
     {
+        PhotonNetwork.NickName = username.text;
+        multiplayerButtonText.text = "Connecting...";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Unir-mos a un Lobby");
         SceneManager.LoadScene("MultiplayerScene");
     }
         
@@ -39,5 +66,15 @@ public class MainMenuManager : MonoBehaviour
         #else
                 Application.Quit();
         #endif
+    }
+    
+    IEnumerator DisconnectPlayer()
+    {
+        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.Disconnect();
+        while (PhotonNetwork.IsConnected)
+        {
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/NetworkingManager.cs
+++ Assets/Scripts/NetworkingManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,48 +10,35 @@ using UnityEngine.UI;
 public class NetworkingManager : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
-    [SerializeField] private Button multiplayerButton;
+    [SerializeField] private GameObject lobbyPanel;
+    [SerializeField] private GameObject roomPanel;
+    [SerializeField] private TMP_InputField roomInputField;
+    [SerializeField] private TextMeshProUGUI roomName;
+    [SerializeField] private Button createRoom;
     void Start()
     {
-        Debug.Log("Connexió a un servidor");
-        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.JoinLobby();
+        roomPanel.SetActive(false);
+        lobbyPanel.SetActive(true);
     }
 
+   
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("Unir-mos a un Lobby");
-        PhotonNetwork.JoinLobby();
-    }
-
-    public override void OnJoinedLobby()
-    {
-        Debug.Log("Estam apunt per jugar!");
-        multiplayerButton.interactable = true;
+        if (roomInputField.text.Length >= 1)
+        {
+            createRoom.interactable = true;
+        }
+        else
+        {
+            createRoom.interactable = false;
+        }
     }
     
    
-    public void FindMatch()
+    public void CreateRoom()
     {
-        Debug.Log("Cercant sala...");
-        PhotonNetwork.JoinRandomRoom();
-        
-    }
-
-    public override void OnJoinRandomFailed(short returnCode, string message)
-    {
-        MakeRoom();
-    }
-
-    public void MakeRoom()
-    {
-        int randomRoomName = Random.Range(0, 5000);
-
         RoomOptions roomOptions = new RoomOptions()
         {
             IsVisible = true,
@@ -59,18 +47,19 @@ public class NetworkingManager : MonoBehaviourPunCallbacks
             PublishUserId = true
         };
 
-        PhotonNetwork.CreateRoom($"RoomName_{randomRoomName}", roomOptions);
-        Debug.Log($"Hem creat la sala{randomRoomName}");
+        PhotonNetwork.CreateRoom(roomInputField.text, roomOptions);
     }
 
     public override void OnJoinedRoom()
     {
-        Debug.Log("Carregar escena del joc mp");
-        PhotonNetwork.LoadLevel("GameOnline");
+        lobbyPanel.SetActive(false);
+        roomPanel.SetActive(true);
+        roomName.text = "Room Name: " + PhotonNetwork.CurrentRoom.Name;
     }
 
     public void Menu()
     {
+        PhotonNetwork.Disconnect();
         SceneManager.LoadScene("MainMenu");
     }
 }
