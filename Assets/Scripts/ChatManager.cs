392df17dfdcba5dc723c5bfee4fac838f1f6d92c
unknown <angelllompart@paucasesnovescifp.cat>
2023-05-03 13:28:33 +0200

    a

---
+++ Assets/Scripts/ChatManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using ExitGames.Client.Photon;
+using Photon.Chat;
+using Photon.Pun;
+using UnityEngine;
+
+public class ChatManager : MonoBehaviour, IChatClientListener
+{
+    private ChatClient _chatClient;
+    [SerializeField] private string userID;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _chatClient = new ChatClient(this);
+        _chatClient.Connect(PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat, PhotonNetwork.AppVersion, new AuthenticationValues(userID));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _chatClient.Service();  
+    }
+
+    public void DebugReturn(DebugLevel level, string message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDisconnected()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnConnected()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnChatStateChange(ChatState state)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnGetMessages(string channelName, string[] senders, object[] messages)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPrivateMessage(string sender, object message, string channelName)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSubscribed(string[] channels, bool[] results)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnsubscribed(string[] channels)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnStatusUpdate(string user, int status, bool gotMessage, object message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUserSubscribed(string channel, string user)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUserUnsubscribed(string channel, string user)
+    {
+        throw new System.NotImplementedException();
+    }
+}
